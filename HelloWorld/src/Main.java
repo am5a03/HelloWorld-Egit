c62b1c4a6b39e330cc2554cce23be5bb207c32de
VM <am5a03@gmail.com>
2012-04-06 15:03:09 +0800

    New VM

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -7,8 +7,6 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("HelloWorld Egit");
-		System.out.println("HeyHey from Raymond");
-		System.out.println("OMG123");
 	}
 
 }
