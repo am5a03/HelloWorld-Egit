6be5891f798d95ac03ad98e2f328763abee27265
Raymond <raymondctc@me.com>
2012-04-09 17:20:06 +0800

    Signed-off-by: Raymond <raymondctc@me.com>

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -7,9 +7,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("HelloWorld Egit");
-		//Sorry, a deleted  second System.out.println("This is a second line");
-		System.out.println("Forget to pull, why you delete the line?");
-		System.out.println("This is a test line");
+		System.out.println("test2");
 	}
 
 }
